a4dbc3641bc29a925e4e1d789010313abc55fd25
Kyle Cheung <kcheung2@ucvts.tec.nj.us>
2019-10-01 09:07:18 -0400

    test

---
+++ apcsatrapezoid.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class apcsatrapezoid {
+    public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+
+      System.out.print("Enter a top value: ");
+      double top = in.nextDouble();
+      System.out.print("Enter a bottom value: ");
+      double bottom = in.nextDouble();
+      System.out.print("Enter a height: ");
+      double height = in.nextDouble();
+
+      double area = ((top + bottom)/2) * height;
+      System.out.println("The area of a trapezoid is " + area + ".");
+
+      in.close();
+    }
+}
